2eff528c277e02e91214a29b71b29e8f444783d3
Bob <bobjeltes@hotmail.com>
2020-07-14 13:09:55 +0200

    Opdr 5 - Threading experimentation

--- Opdr 5 - Threading/Opdr 5 - Threading/Opdr 5 - Threading.cpp
+++ Opdr 5 - Threading/Opdr 5 - Threading/Opdr 5 - Threading.cpp
@@ -2,8 +2,50 @@
 // Bob Jeltes
 
 #include <iostream>
+#include <thread>
+
+void justJoin(std::thread& _thread);
+void justDetach(std::thread& _thread);
+void joinThenDetach(std::thread& _thread);
+void detachThenJoin(std::thread& _thread);
+void hurpadur();
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+
+    // How do I do the thing with the threads?
+    std::thread thread(hurpadur);
+
+    justJoin(thread);
+    //justDetach(thread);
+    //joinThenDetach(thread);
+    //detachThenJoin(thread);
+}
+
+// Geeft GEEN errors?????
+void justJoin(std::thread& _thread) {
+    _thread.join();
+}
+
+// Lijkt niks te doen
+void justDetach(std::thread& _thread) {
+    _thread.detach();
+}
+
+// Geeft error
+void joinThenDetach(std::thread& _thread) {
+    _thread.join();
+    _thread.detach();
+}
+
+// Geeft error
+void detachThenJoin(std::thread& _thread) {
+    _thread.detach();
+    _thread.join();
+}
+
+void hurpadur() {
+    std::cout << "Write a thing here" << std::endl;
 }
