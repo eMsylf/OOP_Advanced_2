9ada9142f9b82b4453016f6970c236dd07119790
Bob <bobjeltes@hotmail.com>
2020-07-14 17:10:32 +0200

    Opdr 5 - Back to basics (Edwin's example)

--- Opdr 5 - Threading/Opdr 5 - Threading/Opdr 5 - Threading.cpp
+++ Opdr 5 - Threading/Opdr 5 - Threading/Opdr 5 - Threading.cpp
@@ -4,48 +4,111 @@
 #include <iostream>
 #include <thread>
 
-void justJoin(std::thread& _thread);
-void justDetach(std::thread& _thread);
-void joinThenDetach(std::thread& _thread);
-void detachThenJoin(std::thread& _thread);
-void hurpadur();
+//void justJoin(std::thread& _thread);
+//void justDetach(std::thread& _thread);
+//void joinThenDetach(std::thread& _thread);
+//void detachThenJoin(std::thread& _thread);
+//void hurpadur();
+////void threadExperiment1();
+//void threadingExampleEdwin();
+//void square(int x);
+
+int exampleNumber = 0;
+
+void square(int x) {
+    std::cout << "Hello from thread " << std::this_thread::get_id() << std::endl;
+    exampleNumber += x * x;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::thread thread(&square, 25);
+    thread.join();
+    //thread.detach();
+    std::cout << "accum = " << exampleNumber << std::endl;
+
+    return 0;
 
 
-    // How do I do the thing with the threads?
-    std::thread thread(hurpadur);
 
-    justJoin(thread);
-    //justDetach(thread);
-    //joinThenDetach(thread);
-    //detachThenJoin(thread);
+    //std::cout << "Hello World!\n";
+    //
+    ////threadExperiment1();
+
+    ////threadingExampleEdwin();
+    //std::thread anotherThread(square, 254);
+
+    ////anotherThread.join();
+    ////anotherThread.detach();
+
+
+    //return 0;
 }
 
-// Geeft GEEN errors?????
-void justJoin(std::thread& _thread) {
-    _thread.join();
-}
-
-// Lijkt niks te doen
-void justDetach(std::thread& _thread) {
-    _thread.detach();
-}
-
-// Geeft error
-void joinThenDetach(std::thread& _thread) {
-    _thread.join();
-    _thread.detach();
-}
-
-// Geeft error
-void detachThenJoin(std::thread& _thread) {
-    _thread.detach();
-    _thread.join();
-}
-
-void hurpadur() {
-    std::cout << "Write a thing here" << std::endl;
-}
+//void threadingExampleEdwin() {
+//    //std::thread anotherThread(&square, 254);
+//
+//    //anotherThread.join();
+//    //anotherThread.detach();
+//
+//    std::cout << "accum = " << exampleNumber << std::endl;
+//}
+//
+//void square(int x) {
+//    std::cout << "Hello from thread" << std::this_thread::get_id() << std::endl;
+//    exampleNumber += x * x;
+//}
+//
+//void threadExperiment1() {
+//    // How do I do the thing with the threads?
+//    std::thread thread1(hurpadur, 25);
+//    std::thread thread2;
+//
+//    std::cout << "Input a number between 0 and 3";
+//    int input;
+//    std::cin >> input;
+//    switch (input)
+//    {
+//    case 0:
+//        justJoin(thread1);
+//        break;
+//    case 1:
+//        justDetach(thread1);
+//        break;
+//    case 2:
+//        joinThenDetach(thread1);
+//        break;
+//    case 3:
+//        detachThenJoin(thread1);
+//        break;
+//    default:
+//        break;
+//    }
+//}
+//
+//// Werkt??
+//void justJoin(std::thread& _thread) {
+//    _thread.join();
+//}
+//
+//// Lijkt niks te doen
+//void justDetach(std::thread& _thread) {
+//    _thread.detach();
+//}
+//
+//// Geeft error
+//void joinThenDetach(std::thread& _thread) {
+//    _thread.join();
+//    _thread.detach();
+//}
+//
+//// Geeft error
+//void detachThenJoin(std::thread& _thread) {
+//    _thread.detach();
+//    _thread.join();
+//}
+//
+//void hurpadur() {
+//
+//    std::cout << "Write a thing here" << std::endl;
+//}
