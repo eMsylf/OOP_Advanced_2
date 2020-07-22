e1541e5f28e4a1e6e1aee40148e945a99b9fb675
eMsylf <bobjeltes@hotmail.com>
2020-07-22 12:14:35 +0200

    Opdr 6 - Werkende shortcut, ZONDER condition_variable! Moet dus nog aan gewerkt worden.

--- Opdr 6 - Producer-consumer/Opdr 6 - Producer-consumer.cpp
+++ Opdr 6 - Producer-consumer/Opdr 6 - Producer-consumer.cpp
@@ -1,10 +1,48 @@
-// Opdr 6 - Producer-consumer
-// Bob Jeltes
-
+#include <queue>
+#include <thread>
 #include <iostream>
+#include <condition_variable>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int counter = 0;
+bool done = false;
+std::queue<int> goods;
 
+std::condition_variable condition;
+std::mutex mutex;
+//std::lock_guard<std::mutex> lock;
+
+void producer() {
+    for (int i = 0; i < 500; ++i) {
+        mutex.lock();
+        goods.push(i);
+        counter++;
+        mutex.unlock();
+    }
+
+    done = true;
+}
+
+void consumer() {
+    while (!done) {
+        while (!goods.empty()) {
+            mutex.lock();
+            goods.pop();
+            counter--;
+            mutex.unlock();
+        }
+    }
+}
+
+int main() {
+    std::thread producerThread(producer);
+    std::thread consumerThread(consumer);
+
+    condition.notify_all();
+    producerThread.join();
+    consumerThread.join();
+
+    std::cout << "Net: " << counter 
+        << std::endl
+        << "Goods queue size: " << goods.size()
+        << std::endl;
 }
