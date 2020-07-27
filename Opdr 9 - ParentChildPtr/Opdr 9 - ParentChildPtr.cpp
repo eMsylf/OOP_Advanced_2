98dca1337abdb6bb6d74bdfab6e332344c860ec9
eMsylf <bobjeltes@hotmail.com>
2020-07-27 12:01:18 +0200

    Opdr 9 - Af

--- Opdr 9 - ParentChildPtr/Opdr 9 - ParentChildPtr.cpp
+++ Opdr 9 - ParentChildPtr/Opdr 9 - ParentChildPtr.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include "Parent.h"
+#include "Opdr 9 - ParentChildPtr.h"
 
 int main() {
     std::shared_ptr<Parent> p1 = std::make_shared<Parent>("Parent1");
     std::cout << "p1: " << *p1 << std::endl;
-    //std::cout << "c1: " << *p1->child;
-
 
     std::shared_ptr<Parent> p2 = std::make_shared<Parent>(*p1); // roept de copy constructor aan
     std::cout << "p2: " << *p2 << std::endl;
-    //std::cout << "c2: " << *p2->child;
+
     std::shared_ptr<Parent> p3 = std::make_shared<Parent>("Parent3");
     std::cout << "p3: " << *p3 << std::endl;
 
     *p3 = *p1; // roept de assignment operator aan
-    std::cout << "p1:" << *p3 << std::endl;
-    
-    //Child* c1 = p1->child.get();
-    //Child* c2 = p2->child.get();
-    //Child* c3 = p3->child.get();
+    std::cout << "p3:" << *p3 << std::endl;
 
-    //delete &p1;
-    //delete &p2;
-    //delete &p3;
-
-    std::shared_ptr<Parent> p4 = std::make_shared<Parent>(*p3);
+    // Stap 2.2
+    std::cout << "Parent 3 use count: "<< p3.use_count() << std::endl;
+    std::shared_ptr<Parent> p4 = p3; // Shared pointer p3 wordt nu 2 keer gebruikt i.p.v. 1 keer
     std::cout << "p4: " << *p4 << std::endl;
+    std::cout << "Parent 3 use count: "<< p3.use_count() << std::endl;
+
+    std::cout << "==== Child names of all parents: ====" << std::endl;
+    std::cout << *p1->child << std::endl;
+    std::cout << *p2->child << std::endl;
+    std::cout << *p3->child << std::endl;
+    std::cout << *p4->child << std::endl;
+    std::cout << "--------" << std::endl;;
+    p1->child.reset();
+    std::cout << "--------" << std::endl;;
+    std::cout << "==== Child names of all parents except p1: ====" << std::endl;
+    std::cout << *p2 << std::endl;
+    std::cout << *p3 << std::endl;
+    std::cout << *p4 << std::endl;
 
     return 0;
 }
